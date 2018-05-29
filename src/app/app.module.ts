65d4ecf59e40b82c2a34eb26599a33aead5c8829
lei_mendenilla <https://lei_mendenilla@bitbucket.org>
2018-05-29 17:47:32 +0800

    fix merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,23 +12,16 @@ import { EvtProvider } from '../providers/evt/evt';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { RegistrationPage } from '../pages/registration/registration';
-<<<<<<< HEAD
 import { ThankYou2Page } from '../pages/thank-you2/thank-you2';
-import { EvtProvider } from '../providers/evt/evt';
-=======
 import { WardrobePage } from '../pages/wardrobe/wardrobe';
 
->>>>>>> dev_lei
 
 
 const pages = [ MyApp,
       HomePage,
       RegistrationPage,
-<<<<<<< HEAD
-      ThankYou2Page
-=======
+      ThankYou2Page,
       WardrobePage
->>>>>>> dev_lei
       ];
 
 @NgModule({
@@ -39,11 +32,8 @@ const pages = [ MyApp,
       links: [
         { component: HomePage, name: 'Home', segment: 'home/:id' },
         { component: RegistrationPage, name: 'RegistrationPage', segment: 'register' },
-<<<<<<< HEAD
-        { component: ThankYou2Page, name: 'ThankYou2Page', segment: 'thank-you-2' }
-=======
+        { component: ThankYou2Page, name: 'ThankYou2Page', segment: 'thank-you-2' },
         { component: WardrobePage, name: 'WardrobePage', segment: 'wardrobe' }
->>>>>>> dev_lei
       ]
     })
   ],
